ebd60de7588f80499f6d291217f2aa858f932778
biologicalwaste <samewas@protonmail.com>
2023-07-14 16:00:33 -0400

    adding read file function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
+use std::fs;
+use std::fs::File;
+use std::io;
 use time::OffsetDateTime;
 
 fn main() {
+    match File::open(".friendstimedata") {
+        Ok(_) => read_file(),
+        Err(_) => {
+            let _ = fs::write(".friendstimedata", "OFFSET:");
+            get_offset();
+        }
+    };
+}
 
-let now = OffsetDateTime::now_utc();
+fn get_offset() {
+    
+}
 
-println!("{now}");
-
+fn read_file() {
+    let mut time_data = String::new();
+    match fs::read_to_string(".friendstimedata") {
+        Ok(data) => time_data = data,
+        Err(_) => println!("File not readable! Is it there? Is it on fire?"),
+    }
+    println!("{time_data}");
 }
