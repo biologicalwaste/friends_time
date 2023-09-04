1f575162434970257ed895e82b1dd70368b30809
biologicalwaste <samewas@protonmail.com>
2023-09-03 20:42:32 -0400

    adding shit to gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use core::num;
 use std::fs;
 use std::fs::File;
-use std::io;
+use std::io::{self, stdin};
 use time::OffsetDateTime;
 
 fn main() {
@@ -9,12 +10,22 @@ fn main() {
         Err(_) => {
             let _ = fs::write(".friendstimedata", "OFFSET:");
             get_offset();
+            read_file();
         }
     };
 }
 
 fn get_offset() {
-    
+    let mut user_offset = String::new();
+    println!("Please input your UTC offset!");
+    let mut offset_in = String::new();
+
+    loop {
+        match stdin().read_line(&mut offset_in) {
+            Ok(_) => break,
+            Err(_) => continue,
+        }
+    }
 }
 
 fn read_file() {
